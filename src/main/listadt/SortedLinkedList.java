3c9ce7306586d731558b9d88a675c843df49c2a1
Ali Madooei <madooei@jhu.edu>
2026-09-13 21:29:43 -0400

    Regenerate starter from updated master

--- src/main/listadt/SortedLinkedList.java
+++ src/main/listadt/SortedLinkedList.java
@@ -14,88 +14,54 @@ public class SortedLinkedList<T extends Comparable<T>> implements SortedList<T> 
   private int size;       // number of real nodes
 
   public SortedLinkedList() {
-    head = new Node<>(null);
-    tail = new Node<>(null);
-    head.next = tail;
-    tail.prev = head;
-    size = 0;
+    // TODO: Implement me
+    throw new UnsupportedOperationException("TODO: Implement me");
   }
 
   @Override
   public void add(T value) {
-    if (value == null) {
-      throw new NullPointerException();
-    }
-    Node<T> current = head.next;
-    while (current != tail && current.value.compareTo(value) <= 0) {
-      current = current.next;
-    }
-    insertBetween(value, current.prev, current);
+    // TODO: Implement me
+    throw new UnsupportedOperationException("TODO: Implement me");
   }
 
   @Override
   public T get(int index) {
-    return node(index).value;
+    // TODO: Implement me
+    throw new UnsupportedOperationException("TODO: Implement me");
   }
 
   @Override
   public int indexOf(T value) {
-    Node<T> current = head.next;
-    int index = 0;
-    while (current != tail) {
-      if (Objects.equals(current.value, value)) {
-        return index;
-      }
-      current = current.next;
-      index++;
-    }
-    return -1;
+    // TODO: Implement me
+    throw new UnsupportedOperationException("TODO: Implement me");
   }
 
   @Override
   public boolean contains(T value) {
-    return indexOf(value) != -1;
+    // TODO: Implement me
+    throw new UnsupportedOperationException("TODO: Implement me");
   }
 
   @Override
   public boolean remove(T value) {
-    Node<T> current = head.next;
-    while (current != tail) {
-      if (Objects.equals(current.value, value)) {
-        current.prev.next = current.next;
-        current.next.prev = current.prev;
-        size--;
-        return true;
-      }
-      current = current.next;
-    }
-    return false;
+    // TODO: Implement me
+    throw new UnsupportedOperationException("TODO: Implement me");
   }
 
   @Override
   public int size() {
-    return size;
+    // TODO: Implement me
+    throw new UnsupportedOperationException("TODO: Implement me");
   }
 
   private Node<T> insertBetween(T value, Node<T> before, Node<T> after) {
-    Node<T> node = new Node<>(value);
-    node.prev = before;
-    node.next = after;
-    before.next = node;
-    after.prev = node;
-    size++;
-    return node;
+    // TODO: Implement me
+    throw new UnsupportedOperationException("TODO: Implement me");
   }
 
   private Node<T> node(int index) {
-    if (index < 0 || index >= size) {
-      throw new IndexOutOfBoundsException();
-    }
-    Node<T> current = head.next;
-    for (int i = 0; i < index; i++) {
-      current = current.next;
-    }
-    return current;
+    // TODO: Implement me
+    throw new UnsupportedOperationException("TODO: Implement me");
   }
 
   private static class Node<T> {
